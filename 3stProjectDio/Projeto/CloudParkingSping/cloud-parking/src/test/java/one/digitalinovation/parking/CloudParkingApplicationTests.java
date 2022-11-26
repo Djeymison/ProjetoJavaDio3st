5404966fe1729dbede2c0180ae3420fed218a0e8
Djeymison <djeymisonwork@gmail.com>
2022-11-25 22:46:17 -0300

    My 1st commit

---
+++ 3stProjectDio/Projeto/CloudParkingSping/cloud-parking/src/test/java/one/digitalinovation/parking/CloudParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinovation.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
